6d0b435bcd2a2cae0fb72e8212f77af47cf21e56
Marc <marcpinson9@gmail.com>
2023-02-17 14:31:16 +0100

    Add chat Hub and clean up client library

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeExploration.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using RealTimeExploration.Hubs;
+
+namespace RealTimeExploration
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
